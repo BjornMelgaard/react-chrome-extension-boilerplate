e4d5ab3200ba3400c52a34a678fc4d6e25c0e546
BjornMelgaard <melgaardbjorn@gmail.com>
2017-11-30 21:10:23 +0200

    not working

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp'
-import gulpCopy from 'gulp-copy'
+import copy from 'gulp-copy'
+import sass from 'gulp-sass'
 import del from 'del'
 import runSequence from 'run-sequence'
+import tildeImporter from 'node-sass-tilde-importer'
 
 // rollup
 import rollup from 'gulp-better-rollup'
@@ -12,15 +14,29 @@ const createRunSequence = (...args) => cb => runSequence(...args, cb)
 
 gulp.task('clean', del.bind(null, ['dist']))
 
+gulp.task('sass', () =>
+  gulp
+    .src('src/popup/styles/**/*.scss')
+    .pipe(
+      sass({
+        importer: tildeImporter,
+        // outputStyle: 'compressed',
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest('dist/popup'))
+)
+
 gulp.task('watch', ['default'], () => {
   gulp.watch('src/**/*', ['default']).on('error', err => {
     console.log(err.toString())
   })
+
+  gulp.watch('src/popup/styles/**/*.scss', ['sass'])
 })
 
 gulp.task('notPopupJs', function() {
   const notPopup = ['src/oauth2/**/*', 'src/popup/index.html', 'src/*']
-  return gulp.src(notPopup).pipe(gulpCopy('dist', { prefix: 1 }))
+  return gulp.src(notPopup).pipe(copy('dist', { prefix: 1 }))
 })
 
 gulp.task('popupJs', () =>
@@ -58,4 +74,4 @@ gulp.task('popupJs', () =>
 
 gulp.task('build', createRunSequence(['notPopupJs', 'popupJs']))
 
-gulp.task('default', ['clean'], createRunSequence('build'))
+gulp.task('default', ['clean'], createRunSequence(['build', 'sass']))
--- src/popup/controllers/showCurrentUser.js
+++ src/popup/controllers/showCurrentUser.js
@@ -1,6 +1,14 @@
 import { getCurrentUser } from '../api'
 
+// const setUserAvatar = avatar => $('.user-page-info>img')
+
+function showUserAvatar({ url, thumb, medium }) {
+  if (!url) {
+  }
+}
+
 export async function showCurrentUser() {
   const currentUser = await getCurrentUser()
+  console.log(currentUser)
   $('#current-user').text(currentUser.username)
 }
